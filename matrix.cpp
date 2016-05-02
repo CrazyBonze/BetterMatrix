81f46e8865b6f2e4456a9e05b453eed9dd0c7998
Michael Taylor <90michaeltaylor@gmail.com>
2016-05-01 18:57:27 -0700

    Added the basic functionality.

--- matrix.cpp
+++ matrix.cpp
@@ -1,13 +1,13 @@
 #include "matrix.h"
 
+// Public Functions
 template <class T>
-Matrix<T>::Matrix()
-  : M_DATA(NULL)
-{ }
-
-template <class T>
-Matrix<T>::Matrix(const unsigned int X, const unsigned int Y)
-  : M_X_DIM(X), M_Y_DIM(Y), M_SIZE(X*Y)
+Matrix<T>::Matrix(const unsigned int row, const unsigned int col)
+  : M_X_DIM       (row),
+    M_Y_DIM       (col),
+    M_SIZE        (row*col),
+    M_DATA        (new T[M_SIZE]),
+    BACK          (M_SIZE - 1)
 { }
 
 template <class T>
@@ -15,7 +15,76 @@ Matrix<T>::~Matrix()
 { }
 
 template <class T>
+void Matrix<T>::print(ostream& os)
+{
+  os << "[";
+  for(unsigned int i = FRONT; i < M_SIZE; i++)
+    os << M_DATA[i] << (i < BACK ? ',':']');
+  os << endl;
+}
+
+template <class T>
+T& Matrix<T>::front()
+{
+  return M_DATA[FRONT];
+}
+
+template <class T>
+T& Matrix<T>::back()
+{
+  return M_DATA[BACK];
+}
+
+template <class T>
+void Matrix<T>::fill(T value)
+{
+  for(unsigned int i = FRONT; i < M_SIZE; i++)
+    M_DATA[i] = value;
+}
+
+template <class T>
 unsigned int Matrix<T>::size() const
 {
   return M_SIZE;
+}
+
+template <class T>
+unsigned int Matrix<T>::X_size() const
+{
+  return M_X_DIM;
+}
+
+template <class T>
+unsigned int Matrix<T>::Y_size() const
+{
+  return M_Y_DIM;
+}
+
+template <class T>
+unsigned int Matrix<T>::row_size() const
+{
+  return X_size();
+}
+
+template <class T>
+unsigned int Matrix<T>::col_size() const
+{
+  return Y_size();
+}
+
+// Private Functions
+
+template <class T>
+unsigned int Matrix<T>::get_index(unsigned int row, unsigned int col) const
+{
+  check_args(row, col);
+  return row * M_Y_DIM + col;
+}
+
+template <class T>
+bool Matrix<T>::check_args(unsigned int row, unsigned int col) const
+{
+  assert(row < M_X_DIM);
+  assert(col < M_Y_DIM);
+  return true;
 }
--- matrix.h
+++ matrix.h
@@ -3,21 +3,37 @@
 
 #include <cassert> //for assert
 #include <cstddef> //for NULL
+#include <iostream> // for ostream
+using namespace std;
 
 template <class T>
 class Matrix
 {
   public:
-    Matrix();
-    Matrix(const unsigned int X, const unsigned int Y);
+    Matrix(const unsigned int row, const unsigned int col);
     ~Matrix();
+    void print(ostream& os);
+    T& front();
+    T& back();
+    void fill(T value);
     unsigned int size() const;
+    unsigned int X_size() const;
+    unsigned int Y_size() const;
+    unsigned int row_size() const;
+    unsigned int col_size() const;
+    T& operator()(unsigned int row, unsigned int col)
+    {
+      return M_DATA[get_index(row, col)];
+    };
   private:
+    unsigned int get_index(unsigned int row, unsigned int col) const;
+    bool check_args(unsigned int row, unsigned int col) const;
     const unsigned int M_X_DIM;
     const unsigned int M_Y_DIM;
     const unsigned int M_SIZE;
-    T *M_DATA;
-
+    T * const M_DATA;
+    const unsigned int FRONT = 0;
+    const unsigned int BACK;
 };
 #include "matrix.cpp"
 #endif //MATRIX_H
